50047eda8671dd2faa25f3b5b046fdead30ef7c3
Ishan R <rathiishan0@gmail.com>
2023-04-02 16:02:23 -0400

    Changed settigns

--- Backend/StockPriceAPI.py
+++ Backend/StockPriceAPI.py
@@ -1,10 +1,12 @@
 from API import API
+
 class StockPriceAPI(API):
     def __init__(self) -> None:
         super().__init__()
     def fetch_data(self, symbol):
         # Implement data fetching from Stock Price API (e.g., Alpha Vantage or Yahoo Finance) using the symbol
         pass
+
     def parse_data(self, data):
         pass
     def set_api_key(self,API_KEY):
