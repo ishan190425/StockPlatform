ee734ae3338e06fba53c2f5c863513e5eefe3129
Ishan R <rathiishan0@gmail.com>
2023-05-05 22:42:28 -0400

    Updated ui

--- stockPlatform/stockPlatform/settings.py
+++ stockPlatform/stockPlatform/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = "django-insecure-qz2_eeuclznd8i0wvghy_!b0yivs3qrrsd^-&&g%bho@-=9&*^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["192.168.68.133","192.168.68.119"]
 
 
 # Application definition
